169823a502763fde9bab61c77823627f8b131553
revathib97 <113500723+revathib97@users.noreply.github.com>
2022-09-21 10:57:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,87 +1,92 @@
-import React,{useEffect, useState} from "react";
-import { Table,TableBody, TableCell, TableHead, TableRow } from "@mui/material";
-import {MenuItem, Select} from "@mui/material";
- 
+import React, { useEffect, useState } from "react";
+import { Table, TableBody, TableCell, TableHead, TableRow } from "@mui/material";
+import { MenuItem, Select } from "@mui/material";
 
 
-function TableData (){
-  const [tableData,setTable]=useState([]);
-  const [sorting,setSorting]=useState({key:"name",ascending:true})
-  const [countriesList,setcountriesList]=useState([]);
+function TableData() {
+  const [tableData, setTable] = useState([]);
+  const [sorting, setSorting] = useState({ key: "name", ascending: true })
+  const [countriesList, setcountriesList] = useState([]);
+  const [filterCountryData, setFilterCountryData] = useState([]);
+  const [selectedCountry, setSelectedCountry]= useState('');
 
+  useEffect(() => {
+    fetch('http://universities.hipolabs.com/search?country')
+      .then((res) => res.json())
+      .then((response) => {
+        setTable(response);
+        setFilterCountryData(response);
+        setcountriesList([...new Set(response.map(e => e.country))])
 
-  useEffect(()=>{
-    fetchTableData();
-    const jsonData =[...tableData]
-    const tableSort=jsonData.sort((a,b)=>{
+      })
+
+  }, [])
+
+  const sortingOnClickHandler = (key, ascending) => {
+
+    
+    const jsonData = [...filterCountryData]
+    const tableSort = jsonData.sort((a, b) => {
       return a[sorting.key].localeCompare(b[sorting.key]);
     })
-    setTable(
+    setFilterCountryData(
       sorting.ascending ? tableSort : tableSort.reverse()
     );
-  },[tableData,sorting])
-  
-  
-  const fetchTableData=()=>{
-    fetch('http://universities.hipolabs.com/search?country')
-    .then((res)=>res.json())
-    .then((response)=>{
-      setTable(response);
-      setcountriesList([...new Set(response.map(e=>e.country))])
-    
-    })
-
-     
+    setSorting({ key: key, ascending: ascending });
   }
-   
 
-  const sortingOnClickHandler =(key,ascending)=>{
-    setSorting({key:key,ascending:ascending}); 
-    
-   
+  const handleCountryChange = (countryName) => {
+    setSelectedCountry(countryName);
+    const tData = tableData.filter(c => c.country.includes(countryName));
+    setFilterCountryData(tData);
   }
-    return(
+
+  return (
     <>
-    <Table>
+      <Table>
         <TableHead>
-            <TableRow>
-            <TableCell style={{cursor:'pointer'}} onClick={()=>sortingOnClickHandler("name",!sorting.ascending)}>Name</TableCell>
+          <TableRow>
+            <TableCell style={{ cursor: 'pointer' }} onClick={() => sortingOnClickHandler("name", !sorting.ascending)}>Name</TableCell>
             <TableCell>
-            <Select style={{ width: "100%" }}   name="country" >
-            
-        {countriesList.map((country,index)=>
-          <MenuItem value={country}>{country}</MenuItem>
-         
-        )}
-        
-      </Select>
-        </TableCell>
+              <Select
+                style={{ width: "100%" }}
+                value={selectedCountry}
+                onChange={e => handleCountryChange(e.target.value)}
+                name="country"
+              >
+                {countriesList.map((country, index) => {
+                  return (
+                    <MenuItem key={index} value={country}>{country}</MenuItem>
+                  )
+                })}
+              </Select>
+            </TableCell>
             <TableCell>Country Code</TableCell>
             <TableCell>Domains</TableCell>
-            <TableCell>Web_pages</TableCell>  
+            <TableCell>Web_pages</TableCell>
             <TableCell>state_province</TableCell>
-                
-            </TableRow>
+
+          </TableRow>
         </TableHead>
-        
+
         <TableBody>
-        {tableData.map((data, index)=>{
-                    return(
-            <TableRow key={index+1}>
-        
+          {filterCountryData.map((data, index) => {
+            return (
+              <TableRow key={index + 1}>
+
                 <TableCell>{data.name}</TableCell>
                 <TableCell>{data.country}</TableCell>
                 <TableCell>{data.alpha_two_code}</TableCell>
                 <TableCell>{data.domains}</TableCell>
                 <TableCell>{data.web_pages}</TableCell>
-                <TableCell>{data.state_province}</TableCell> 
-               
-            </TableRow>
-               )
-            })}
+                <TableCell>{data.state_province}</TableCell>
+
+              </TableRow>
+            )
+          })}
         </TableBody>
-    </Table>  
-        </>
-    )
+      </Table>
+    </>
+  )
 }
 export default TableData;
